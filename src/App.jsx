a259db09e56034d185b2a71ed754a859303f15ba
Dawid Metelski <metel628@onet.pl>
2023-04-20 10:58:04 +0200

    detecting and handling game over

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { gameSubject, initGame } from './components/Game';
+import { gameSubject, initGame, resetGame } from './components/Game';
 import Board from './components/Board';
 
 function App() {
 
   const [board, setBoard] = useState([])
+  const [isGameOver, setIsGameOver] = useState()
+  const [result, setResult] = useState()
 
   useEffect(() => {
     initGame()
-    const subscribe = gameSubject.subscribe(game => setBoard(game.board))
+    const subscribe = gameSubject.subscribe(game => {
+      setBoard(game.board)
+      setIsGameOver(game.isGameOver)
+      setResult(game.result)
+    })
     return () => subscribe.unsubscribe()
   }, [])
 
   return (
     <div className="container">
+      {
+        isGameOver && (
+          <h2 className="vertical-text">
+            GAME OVER
+            <button onClick={resetGame}><span>NEW GAME</span></button>
+        </h2>
+      )}
       <div className="board-container">
         <Board board={board}/>
       </div>
+      {result && <p className="vertical-text">{result}</p>}
     </div>
   );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,58 +1,81 @@
-import { Chess } from 'chess.js';
+import * as Chess from 'chess.js'
 import { BehaviorSubject } from 'rxjs'
 
-
 const chess = new Chess()
-let promotion = 'rnb2bnr/pppPkppp/8/4p3/7q/8/PPPP1PPP/RNBQKBNR w KQ - 1 5'
-
 
 export const gameSubject = new BehaviorSubject()
 
-// ruch figury
-export function move(from, to, promotion) {
-    let tempMove = {from, to}
-    if(promotion) {
-        tempMove.promotion = promotion
-    }
-
-    try {
-        // czy ruch jest mozliwy?
-        const legalMove = chess.move(tempMove)
-        // wykonaj ruch
-        if(legalMove){
-           updateGame() 
-        }
-    } catch (e) {
-        console.log('Wrong move')
-    }
-}
-
-// aktualizacja szachownicy
-const updateGame = (pendingPromotion) => {
-  const newGame = {
-    board: chess.board(),
-    pendingPromotion
-  }
-  gameSubject.next(newGame)
-}
-
-// start gry
 export function initGame() {
-  updateGame()
+    const savedGame = localStorage.getItem('savedGame')
+    if (savedGame) {
+        chess.load(savedGame)
+    }
+    updateGame()
 }
- 
+
+export function resetGame() {
+    chess.reset()
+    updateGame()
+}
 
 export function handleMove(from, to) {
-    // czy ma promocje
-    const promotions = chess.moves({verbose: true}).filter(m => m.promotion)
+    // czy dany ruch ma promocje?
+    const promotions = chess.moves({ verbose: true }).filter(m => m.promotion)
     console.table(promotions)
-    if(promotions.some(p => `${p.from}:${p.to}` === `${from}:${to}`)) {
+    if (promotions.some(p => `${p.from}:${p.to}` === `${from}:${to}`)) {
         const pendingPromotion = { from, to, color: promotions[0].color }
         updateGame(pendingPromotion)
     }
-    const {pendingPromotion} = gameSubject.getValue()
-    // if(!pendingPromotion) {
-    //     move(from, to)
-    // }
+    const { pendingPromotion } = gameSubject.getValue()
+
     move(from, to)
 }
+
+
+export function move(from, to, promotion) {
+    let tempMove = { from, to }
+    if (promotion) {
+        tempMove.promotion = promotion
+    }
+    const legalMove = chess.move(tempMove)
+
+    if (legalMove) {
+        updateGame()
+    }
+}
+
+const updateGame = pendingPromotion => {
+    const isGameOver = chess.game_over()
+
+    const newGame = {
+        board: chess.board(),
+        pendingPromotion,
+        isGameOver,
+        turn: chess.turn(),
+        result: isGameOver ? getGameResult() : null
+    }
+
+    localStorage.setItem('savedGame', chess.fen())
+
+    gameSubject.next(newGame)
+}
+
+const getGameResult = () => {
+    if (chess.in_checkmate()) {
+        const winner = chess.turn() === "w" ? 'BLACK' : 'WHITE'
+        return `CHECKMATE - ${winner} WON`
+    } else if (chess.in_draw()) {
+        let reason = '50 - MOVES - RULE'
+        if (chess.in_stalemate()) {
+            reason = 'STALEMATE'
+        } else if (chess.in_threefold_repetition()) {
+            reason = 'REPEATED MOVE 3 TIMES'
+        } else if (chess.insufficient_material()) {
+            reason = "NOT ENOUGH MATERIAL"
+        }
+        return `DRAW - ${reason}`
+    } else {
+        return 'UNKNOWN REASON'
+    }
+}
+
